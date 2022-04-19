8143a072c924db2c81556d5fa719854767961956
Pato <99479428+patrickateng@users.noreply.github.com>
2022-04-19 17:55:55 +0300

    team task

--- get_flag.c
+++ get_flag.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * get_flag - turns on flags if _printf finds
@@ -10,23 +10,23 @@
  */
 int get_flag(char s, flags_t *f)
 {
-	int i = 0;
+    int i = 0;
 
-	switch (s)
-	{
-		case '+':
-			f->plus = 1;
-			i = 1;
-			break;
-		case ' ':
-			f->space = 1;
-			i = 1;
-			break;
-		case '#':
-			f->hash = 1;
-			i = 1;
-			break;
-	}
+    switch (s)
+    {
+    case '+':
+        f->plus = 1;
+        i = 1;
+        break;
+    case ' ':
+        f->space = 1;
+        i = 1;
+        break;
+    case '#':
+        f->hash = 1;
+        i = 1;
+        break;
+    }
 
-	return (i);
+    return (i);
 }
